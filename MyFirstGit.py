3134c3e30136d89ba3039175a76bb8239e079770
aryasreegs <134701365+aryasreegs@users.noreply.github.com>
2023-06-24 14:41:10 +0100

    This is my second try

--- MyFirstGit.py
+++ MyFirstGit.py
@@ -1,3 +1,10 @@
 print("Hi python")
 print("My first python code to git")
-print("My firt Github change is going to my local")
+print("My first Github change is going to my local")
+print("Slowly learning python")
+s1 = "Happy "
+s2 = "to "
+s3 = "learn "
+s4 = "python"
+print(s1 + s2 + s3 + s4 + "!!!")
+
